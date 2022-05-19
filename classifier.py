ea043859aea5d73c980a135f47652a59aff419ec
Harshil-bit <75490038+Harshil-bit@users.noreply.github.com>
2022-05-19 19:49:45 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -6,7 +6,8 @@ from sklearn.linear_model import LogisticRegression
 from PIL import Image
 import PIL.ImageOps
 
-X,y= fetch_openml('mnist_784', version=1, return_X_y=True)
+X=np.load('image.npz')['arr_0']
+y=pd.read_csv("labels.csv)("labels")
 X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=9,train_size=7500,test_size=2500)
 
 #Scaling
